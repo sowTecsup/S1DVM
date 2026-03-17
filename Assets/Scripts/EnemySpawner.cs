15bf9e6feb4861a325804731b94aa8de1dc323af
sowTecsup <jneyra@tecsup.edu.pe>
2026-03-17 10:30:47 -0500

    2

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,13 +7,39 @@ public class EnemySpawner : MonoBehaviour
     public float spawnInterval = 2f;
     public bool EnableSpawner;
 
+    public float counter;
+
     void Start()
     {
         
     }
     void Update()
     {
-        
+        if (EnableSpawner)
+        {
+            counter += Time.deltaTime;
+            if (counter > spawnInterval)
+            {
+                GameObject obj = Instantiate(EnemyPrefab,transform);
+               
+                
+                Vector3 origin = transform.position;
+
+                Vector3 dir = new Vector3(Random.Range(-1f,1f) ,0, Random.Range(-1f, 1f)).normalized;
+
+
+
+                Vector3 FinalPosition = origin + dir * Random.Range(0, range);
+
+
+
+                obj.transform.position = FinalPosition;
+
+
+                counter = 0f;
+
+            }
+        }
     }
     public void OnTriggerEnter(Collider other)
     {
